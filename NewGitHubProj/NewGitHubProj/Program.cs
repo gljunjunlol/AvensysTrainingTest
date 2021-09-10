2bd4822c38b59a90e63e3c76d5c6bf84ab751141
olivepenguin <83709744+olivepenguin@users.noreply.github.com>
2021-09-10 16:56:37 +0800

    Initial checking

---
+++ NewGitHubProj/NewGitHubProj/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewGitHubProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello again!");
+            Console.ReadLine();
+        }
+    }
+}
